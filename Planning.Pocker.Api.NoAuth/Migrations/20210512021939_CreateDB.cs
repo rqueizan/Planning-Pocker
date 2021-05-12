37a5c2835b61afce3c6d15e0591fb51cab286c2b
Rydem <rqueizan@outlook.com>
2021-05-11 22:21:50 -0400

    #1 [Yo] Create DB, initial migration

---
+++ Planning.Pocker.Api.NoAuth/Migrations/20210512021939_CreateDB.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Planning.Pocker.Api.NoAuth.Migrations
+{
+    public partial class CreateDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Carta",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Valor = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carta", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Historial",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Historial", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Voto",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UsuarioId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CartaId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    HistorialId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Voto", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Voto_Carta_CartaId",
+                        column: x => x.CartaId,
+                        principalTable: "Carta",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Voto_Historial_HistorialId",
+                        column: x => x.HistorialId,
+                        principalTable: "Historial",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Voto_Usuario_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Voto_CartaId",
+                table: "Voto",
+                column: "CartaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Voto_HistorialId",
+                table: "Voto",
+                column: "HistorialId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Voto_UsuarioId",
+                table: "Voto",
+                column: "UsuarioId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Voto");
+
+            migrationBuilder.DropTable(
+                name: "Carta");
+
+            migrationBuilder.DropTable(
+                name: "Historial");
+
+            migrationBuilder.DropTable(
+                name: "Usuario");
+        }
+    }
+}
